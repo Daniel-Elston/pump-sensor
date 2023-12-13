55b4860fdc50ef87e262c0f023fabe58d13b8fce
Daniel Thomas Elston <delstonds@outlook.com>
2023-12-13 23:15:39 +0000

    Add Argparse: Pass argument to view data by index

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -1,21 +1,20 @@
 # -*- coding: utf-8 -*-
 from __future__ import annotations
 
+import argparse
 import os
 from pathlib import Path
 
 import dotenv
 import pandas as pd
 import torch
+from torch.utils.data import DataLoader
 from torch.utils.data import Dataset
-# import argparse
-# import sys
 
 print(torch.__version__)  # PyTorch version = 2.2.0
 
 project_dir = Path(__file__).resolve().parents[2]
-dotenv_path = os.path.join(project_dir, '.env')
-dotenv.load_dotenv(dotenv_path)
+dotenv.load_dotenv(os.path.join(project_dir, '.env'))
 
 
 class SensorDataset(Dataset):
@@ -32,13 +31,16 @@ class SensorDataset(Dataset):
         # Load the data, transform and select
         self.sensor_data = pd.read_csv(csv_file)
 
-        # Separate timestamps
+        # Separate timestamps for display/plotting
         self.timestamps = self.sensor_data.iloc[:, 1]
 
         self.transform = transform
-        self.data = self.sensor_data.iloc[:, 2:-1]  # Select only sensor data
+        self.data = self.sensor_data.iloc[:, 2:-1]  # Just sensor data
 
     def __len__(self):
+        """
+        Return the total number of samples in the dataset. Each sample is a sequence of data points.
+        """
         return len(self.sensor_data)
 
     def __getitem__(self, idx):
@@ -63,21 +65,29 @@ class SensorDataset(Dataset):
         return torch.tensor(sequence.values, dtype=torch.float32), self.timestamps.iloc[idx]
 
 
-def main():
-
+def main(index=None):
     path_to_csv_file = os.path.join(project_dir, 'data/raw/sensor.csv')
     dataset = SensorDataset(csv_file=path_to_csv_file)
-    dataloader = torch.utils.data.DataLoader(
-        dataset, batch_size=4, shuffle=True)
+    dataloader = DataLoader(dataset, batch_size=4, shuffle=True)
 
-    for i, (batch, timestamps) in enumerate(dataloader):
-        print(f"Batch {i+1}:")
-        print(batch, timestamps)
-
-        # Break after printing a few batches
-        if i == 2:
-            break
+    if index is not None:
+        # Retrieve a single item using __getitem__
+        data, timestamp = dataset[index]
+        print(f"Data at index {index}: {data}")
+        print(f"Timestamp: {timestamp}")
+    else:
+        # Default behavior: iterate through the DataLoader
+        for i, (batch, timestamps) in enumerate(dataloader):
+            print(f"Batch {i+1}:")
+            print(batch, timestamps)
+            if i == 2:
+                break
 
 
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser(description="Process some integers.")
+    parser.add_argument('--index', type=int,
+                        help='Index of the item to retrieve', required=False)
+    args = parser.parse_args()
+
+    main(index=args.index)
